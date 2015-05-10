cd43b5ba149a57efd71b3c680d5907bd0a7971fd
bliti <pablo.rivera.programmer@gmail.com>
2015-05-10 10:27:02 -0400

    adding website views and updating template style

--- website/urls.py
+++ website/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
-from .views import OrganizationView
+from .views import OrganizationView, EmployeeView, EmployeeClockView
 
 urlpatterns = [
-    url(r'^', OrganizationView.as_view(), name="organization-view"),
+    url(r'^employee/clock/$', EmployeeClockView.as_view(), name="employee-clock-view"),
+    url(r'^employee/$', EmployeeView.as_view(), name="employee-view"),
+    url(r'^$', OrganizationView.as_view(), name="organization-view"),
 ]
--- website/views.py
+++ website/views.py
@@ -1,5 +1,13 @@
 from django.views.generic import TemplateView
 
 
+class EmployeeView(TemplateView):
+    template_name = "employee.html"
+
+
+class EmployeeClockView(TemplateView):
+    template_name = "employee-clock.html"
+
+
 class OrganizationView(TemplateView):
     template_name = "company.html"
