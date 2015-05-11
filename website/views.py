7d9e7d9f57e39992a4f32bf7ab92466d4bff6525
bliti <pablo.rivera.programmer@gmail.com>
2015-05-10 21:26:45 -0400

    organization sign in is completed.

--- website/views.py
+++ website/views.py
@@ -1,6 +1,33 @@
 from django.core.urlresolvers import reverse_lazy
+from django.contrib import messages
+from django.core.exceptions import ObjectDoesNotExist, MultipleObjectsReturned
 from django.views.generic import TemplateView, FormView
+from django.shortcuts import redirect
 from organizations.forms import OrganizationForm
+from organizations.models import Organization
+
+
+class OrganizationView(FormView):
+    template_name = "organization.html"
+    form_class = OrganizationForm
+    success_url = reverse_lazy('employee-view')
+
+    def form_valid(self, form):
+        try:
+            organization = Organization.objects.get(name=form.cleaned_data['name'])
+            self.request.session['organization_name'] = organization.name
+            return super(OrganizationView, self).form_valid(form)
+        
+        except ObjectDoesNotExist:
+            messages.add_message(self.request, messages.WARNING, 'Incorrect Organization Credentials')
+            return redirect(reverse_lazy('organization-view'))
+        
+        except MultipleObjectsReturned:
+            messages.add_message(self.request, messages.WARNING, 'There was an error in your request. Please try again.')
+            return redirect(reverse_lazy('organization-view'))
+        
+        messages.add_message(self.request, messages.WARNING, 'Something went wrong. Please try again.')
+        return redirect(reverse_lazy('organization-view'))
 
 
 class EmployeeView(TemplateView):
@@ -9,15 +36,4 @@ class EmployeeView(TemplateView):
 
 class EmployeeClockView(TemplateView):
     template_name = "employee-clock.html"
-
-
-class OrganizationView(FormView):
-    template_name = "company.html"
-    form_class = OrganizationForm
-    success_url = reverse_lazy('employee-view')
-    
-    
-    def form_valid(self, form):
-        #process the form here!
-        #pasuign for mother's day. :D
-        return super(OrganizationView, self).form_valid(form)
+                   
