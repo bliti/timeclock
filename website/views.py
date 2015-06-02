4dc3a8140489b556938aa86ac4e71de29cf14ee7
bliti <pablo.rivera.programmer@gmail.com>
2015-06-01 22:39:12 -0400

    timezone not showing up properly fixed by setting USE_TC=FALSE

--- website/views.py
+++ website/views.py
@@ -4,6 +4,7 @@ from django.core.exceptions import ObjectDoesNotExist, MultipleObjectsReturned
 from django.views.generic import View, TemplateView, FormView
 from django.shortcuts import redirect, render
 from django.utils import timezone
+from django.conf import settings
 from organizations.forms import OrganizationLoginForm
 from organizations.models import Organization
 from employees.forms import EmployeeLoginForm
